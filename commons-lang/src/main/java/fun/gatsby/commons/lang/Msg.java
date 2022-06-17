0860ac396191330bde7f4683c61aa2becf95d274
gatsby <civin@bupt.cn>
2022-06-17 22:10:08 +0800

    update

--- commons-lang/src/main/java/fun/gatsby/commons/lang/Msg.java
+++ commons-lang/src/main/java/fun/gatsby/commons/lang/Msg.java
@@ -10,14 +10,14 @@ import java.io.Serializable;
  * @author Civin
  */
 @Data
-public class Msg<T extends Serializable> {
+public class Msg<T> implements Serializable {
 
     public static int CODE_SUCCESS = 100;
     public static int CODE_FAIL = 200;
     public static String MSG_SUCCESS = "process successed";
     public static String MSG_FAIL = "process failed";
     //返回的数据
-    T date;
+    T data;
     //状态码 100-成功 200-失败
     private int code;
     //提示信息
@@ -26,20 +26,20 @@ public class Msg<T extends Serializable> {
     private Msg() {
     }
 
-    public static <T extends Serializable> Msg<T> success(T data) {
-        Msg<T> msg = new Msg<>();
-        msg.code = CODE_SUCCESS;
-        msg.msg = MSG_SUCCESS;
-        msg.date = data;
-        return msg;
+    public static <T> Msg<T> of(String msg, int code, T data) {
+        var dto = new Msg<T>();
+        dto.data = data;
+        dto.msg = msg;
+        dto.code = code;
+        return dto;
     }
 
-    public static <T extends Serializable> Msg<T> fail(T data) {
-        Msg<T> msg = new Msg<>();
-        msg.code = CODE_FAIL;
-        msg.msg = MSG_FAIL;
-        msg.date = data;
-        return msg;
+    public static <T> Msg<T> ok(T data) {
+        return of(MSG_SUCCESS, CODE_SUCCESS, data);
+    }
+
+    public static <T> Msg<T> fail(T data) {
+        return of(MSG_FAIL, CODE_SUCCESS, data);
     }
 }
 
---
+++ commons-lang/src/test/java/fun/gatsby/commons/lang/MsgTest.java
@@ -0,0 +1,14 @@
+package fun.gatsby.commons.lang;
+
+import junit.framework.TestCase;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MsgTest extends TestCase {
+
+    public void testOf() {
+        var msg =
+                Msg.of("123", 0, System.out.getClass());
+        log.debug("{}", Msg.ok(msg));
+    }
+}
